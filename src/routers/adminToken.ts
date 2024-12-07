666620ce70702741523d941dba54e0f72ff8746f
Revant-S <146692646+Revant-S@users.noreply.github.com>
2024-12-07 10:58:30 +0530

    Update adminToken.ts

--- src/routers/adminToken.ts
+++ src/routers/adminToken.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as adminTokenHandlers from "../contorllers/adminToken"
 
+
 const router = Router()
 
 router.post("/getAdminToken", adminTokenHandlers.getAdminToken)
